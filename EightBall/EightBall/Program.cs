d81a517cd660b0a50a47ed6a9745807a20caedc5
5unagawa <s2885524@griffithuni.edu.au>
2017-01-12 23:57:41 +1000

    Update Program.cs

--- EightBall/EightBall/Program.cs
+++ EightBall/EightBall/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,29 @@ namespace EightBall
     {
         static void Main(string[] args)
         {
+            bool state = false;
+            string fileLoc = ("f:\\8ball.txt");
+            string[] responses = GetResponses(fileLoc);
+            Console.WriteLine("5unagawa's all-knowing 8-Ball.\nAsk it any question you seek an answer to...");
+            while (state == false)
+            {
+                Console.ReadLine();
+                Answer(responses);
+            }
+        }
+
+        static string[] GetResponses(string fileLoc)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(fileLoc);
+            string[] responses = System.IO.File.ReadAllLines(fileLoc);
+            return responses;
+        }
+
+        static void Answer(string[] responses)
+        {
+            var rand = new Random();
+            int pos = rand.Next(0, responses.Length);
+            Console.WriteLine(responses[pos]);
         }
     }
 }
